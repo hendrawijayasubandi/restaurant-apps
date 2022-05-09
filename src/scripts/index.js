5ba718e3f08b7b580feaa19db8506dcfe3161899
hendrawijayasubandi <hendra.w.subandi@gmail.com>
2022-05-09 14:15:04 +0700

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,15 +7,15 @@ const hero = document.querySelector('.hero');
 const main = document.querySelector('main');
 const drawer = document.querySelector('#drawer');
 
-menu.addEventListener('click', function (event) {
-    drawer.classList.toggle('open');
-    event.stopPropagation()
+menu.addEventListener('click', (event) => {
+  drawer.classList.toggle('open');
+  event.stopPropagation();
 });
 
-hero.addEventListener('click', function () {
-    drawer.classList.remove('open');
+hero.addEventListener('click', () => {
+  drawer.classList.remove('open');
 });
 
-main.addEventListener('click', function () {
-    drawer.classList.remove('open');
+main.addEventListener('click', () => {
+  drawer.classList.remove('open');
 });
