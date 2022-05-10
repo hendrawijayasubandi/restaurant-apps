36a802d40ad112b128f9cb97e58c75644a1717d5
hendrawijayasubandi <hendra.w.subandi@gmail.com>
2022-05-10 13:04:41 +0700

    update

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'YOUR_API_KEY',
+  BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,9 +1,11 @@
 import NowRestaurant from '../views/pages/home';
+import Favorite from '../views/pages/favorite';
 import Detail from '../views/pages/detail';
 
 const routes = {
   '/': NowRestaurant, // default page
   '/now-restaurant': NowRestaurant,
+  '/favorite' : Favorite,
   '/detail/:id': Detail,
 };
 
