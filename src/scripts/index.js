88dd83b2dab437894c9dbce75048dbfac2684b26
Ibnu Choldun <96803344+ibnucholdun@users.noreply.github.com>
2022-05-10 10:40:36 +0700

    update file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,32 +1,14 @@
 import 'regenerator-runtime';
 import '../styles/main.css';
 import '../styles/responsive.css';
-import App from '../views/app';
+import App from './views/apps';
 
 const app = new App({
-  button: document.querySelector('#hamburgerButton'),
-  drawer: document.querySelector('#navigationDrawer'),
-  content: document.querySelector('#mainContent'),
+  button: document.querySelector('#menu'),
+  drawer: document.querySelector('#drawer'),
+  content: document.querySelector('#maincontent'),
 });
 
 window.addEventListener('hashchange', () => {
   app.renderPage();
-});
-
-const menu = document.querySelector('#menu');
-const hero = document.querySelector('.hero');
-const main = document.querySelector('main');
-const drawer = document.querySelector('#drawer');
-
-menu.addEventListener('click', (event) => {
-  drawer.classList.toggle('open');
-  event.stopPropagation();
-});
-
-hero.addEventListener('click', () => {
-  drawer.classList.remove('open');
-});
-
-main.addEventListener('click', () => {
-  drawer.classList.remove('open');
 });
