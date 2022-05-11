e5bf0cc0dbf938984187f6b703b284fbdf75e05a
hendrawijayasubandi <hendra.w.subandi@gmail.com>
2022-05-11 09:50:04 +0700

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ import 'regenerator-runtime';
 import '../styles/main.css';
 import '../styles/responsive.css';
 import App from './views/apps';
+import swRegister from './utils/sw-register';
 
 const app = new App({
   button: document.querySelector('#menu'),
