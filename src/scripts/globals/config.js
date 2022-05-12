7636c6e82e23bfac04849beed0351b160d2a0e30
hendrawijayasubandi <hendra.w.subandi@gmail.com>
2022-05-12 17:25:21 +0700

    update

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,6 +2,7 @@ const CONFIG = {
   KEY: '',
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
+  CACHE_NAME: new Date().toISOString(),,
 };
 
 export default CONFIG;
