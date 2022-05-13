cfc837e77adc086ada33f58e114843193f1637e8
hendrawijayasubandi <hendra.w.subandi@gmail.com>
2022-05-13 09:11:53 +0700

    update

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,12 +1,11 @@
 import NowRestaurant from '../views/pages/home';
-import Favorite from '../views/pages/favorite';
 import Detail from '../views/pages/detail';
 import Like from '../views/pages/like';
 
 const routes = {
   '/': NowRestaurant, // default page
   '/now-restaurant': NowRestaurant,
-  '/favorite' : Favorite,
+  '/favorite' : Like,
   '/detail/:id': Detail,
 };
 
