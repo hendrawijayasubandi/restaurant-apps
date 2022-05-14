a6481a3ae13df2f3958968a8bc6836f6a870f666
hendrawijayasubandi <hendra.w.subandi@gmail.com>
2022-05-14 14:11:45 +0700

    update

--- src/scripts/views/pages/like.js
+++ src/scripts/views/pages/like.js
@@ -1,24 +1,18 @@
 import FavoriteRestaurantIdb from '../../data/restaurantapps-idb';
-import { createRestaurantItemTemplate } from '../templates/template-owner';
+import FavoriteRestaurantSearchView from './liked-restaurants/favorite-restaurant-search-view';
+import FavoriteRestaurantShowPresenter from './liked-restaurants/favorite-restaurant-show-presenter';
+import FavoriteRestaurantSearchPresenter from './liked-restaurants/favorite-restaurant-search-presenter';
+
+const view = new FavoriteRestaurantSearchView();
 
 const Like = {
   async render() {
-    return `
-    <div class="content">
-    <h2 class="content__heading">Your Liked Restaurant</h2>
-      <div id="restaurant" class="restaurant">
-
-      </div>
-    <div>
- `;
+    return view.getTemplate();
   },
 
   async afterRender() {
-    const restaurants = await FavoriteRestaurantIdb.getAllRestaurants();
-    const restaurantsContainer = document.querySelector('#restaurant');
-    restaurants.forEach((restaurant) => {
-      restaurantsContainer.innerHTML += createRestaurantItemTemplate(restaurant);
-    });
+    new FavoriteRestaurantShowPresenter({ view, favoriteRestaurants: FavoriteRestaurantIdb });
+    new FavoriteRestaurantSearchPresenter({ view, favoriteRestaurants: FavoriteRestaurantIdb });
   },
 };
 
