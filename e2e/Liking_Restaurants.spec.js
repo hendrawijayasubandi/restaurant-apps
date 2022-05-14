15759a6703a070310b3c587504195ee5adeb7200
hendrawijayasubandi <hendra.w.subandi@gmail.com>
2022-05-14 15:10:06 +0700

    update

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,18 +1,70 @@
-Feature('Liking Movies');
+const assert = require('assert');
+
+Feature('Liking Restaurants');
 
 Before((I) => {
   I.amOnPage('/#/like');
 });
 
-Scenario('showing empty liked movies', (I) => {
+Scenario('showing empty liked restaurants', (I) => {
   I.seeElement('#query');
   // I.seeElement('.query'); // membuat test menjadi gagal
-  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+  I.see('Tidak ada food untuk ditampilkan', '.restaurant-item__not__found');
 });
 
-Scenario('liking one movie', (I) => {
-  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+Scenario('liking one restaurant', async (I) => {
+    I.see('Tidak ada food untuk ditampilkan', '.restaurant-item__not__found');
+  
+    I.amOnPage('/');
+  
+    I.seeElement('.restaurant__title a');
+    
+    const firstFood = locate('.restaurant__title a').first();
+    const firstFoodTitle = await I.grabTextFrom(firstFood);
+    I.click(firstFood);
+  
+    I.seeElement('#likeButton');
+    I.click('#likeButton');
+  
+    I.amOnPage('/#/like');
+    I.seeElement('.restaurant-item');
 
-  I.amOnPage('/');
-  // … kita akan mengisi uji coba berikutnya …
+    const likedFoodTitle = await I.grabTextFrom('.restaurant__title');
+
+    assert.strictEqual(firstFoodTitle, likedFoodTitle);
+});
+
+Scenario('searching restaurants', async (I) => {
+    I.see('Tidak ada film untuk ditampilkan', '.restaurant-item__not__found');
+  
+    I.amOnPage('/');
+  
+    I.seeElement('.restaurant__title a');
+  
+    const titles = [];
+  
+    for (let i = 1; i <= 3; i++) {
+      I.click(locate('.restaurant__title a').at(i));
+      I.seeElement('#likeButton');
+      I.click('#likeButton');
+      titles.push(await I.grabTextFrom('.restaurant__title'));
+      I.amOnPage('/');
+    }
+  
+    I.amOnPage('/#/like');
+    I.seeElement('#query');
+  
+    const searchQuery = titles[1].substring(1, 3);
+    const matchingRestaurants = titles.filter((title) => title.indexOf(searchQuery) !== -1);
+  
+    I.fillField('#query', searchQuery);
+    I.pressKey('Enter');
+  
+    const visibleLikedRestaurants = await I.grabNumberOfVisibleElements('.restaurant-item');
+    assert.strictEqual(matchingRestaurants.length, visibleLikedRestaurants);
+  
+    matchingRestaurants.forEach(async (title, index) => {
+      const visibleTitle = await I.grabTextFrom(locate('.restaurant__title').at(index + 1));
+      assert.strictEqual(title, visibleTitle);
+    });
 });
--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,19 @@
-// Feature('Liking Movies');
+// Feature('Liking Restaurants');
+// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+browsers: ['Chrome'],
 
 Before((I) => {
   I.amOnPage('/#/like');
 });
 
-Scenario('showing empty liked movies', (I) => {
+Scenario('showing empty liked restaurants', (I) => {
   I.seeElement('#query');
   // I.seeElement('.query'); // membuat test menjadi gagal
-  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+  I.see('Tidak ada film untuk ditampilkan', '.restaurant-item__not__found');
 });
 
-Scenario('liking one movie', (I) => {
-  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+Scenario('liking one restaurant', (I) => {
+  I.see('Tidak ada film untuk ditampilkan', '.restaurant-item__not__found');
 
   I.amOnPage('/');
   // … kita akan mengisi uji coba berikutnya …
