bfce227afd76e97146a23d7af2514395f25dec54
hendrawijayasubandi <hendra.w.subandi@gmail.com>
2022-05-14 13:41:28 +0700

    update

--- src/scripts/views/pages/liked-restaurant/favorite-restaurant-search-view.js
+++ src/scripts/views/pages/liked-restaurant/favorite-restaurant-search-view.js
@@ -1,17 +1,30 @@
+import { createRestaurantItemTemplate } from '../../templates/template-creator';
+
 class FavoriteRestaurantSearchView {
     getTemplate() {
       return `
-          <div id="restaurant-search-container">
-              <input id="query" type="text">
-              <div class="restaurant-result-container">
-                  <ul class="restaurant">
-                  </ul>
-              </div>
-          </div>
-          `;
+      <div id="movie-search-container">
+      <input id="query" type="text">
+      <div class="movie-result-container">
+          <ul class="restaurants">
+          </ul>
+      </div>
+  </div>
+  `;
+}
+
+    getFavoriteMovieTemplate() {
+    return `
+    <div class="content">
+        <h2 class="content__heading">Your Liked Movie</h2>
+        <div id="restaurants" class="restaurants">
+        </div>
+    </div>
+    `;
     }
   
     runWhenUserIsSearching(callback) {
+
       document.getElementById('query').addEventListener('change', (event) => {
         callback(event.target.value);
       });
@@ -25,7 +38,7 @@ class FavoriteRestaurantSearchView {
           '',
         );
       } else {
-        html = '<div class="restaurant__not__found">Film tidak ditemukan</div>';
+        html = '<div class="restaurant__not__found">Food tidak ditemukan</div>';
       }
   
       document.querySelector('.restaurant').innerHTML = html;
@@ -33,6 +46,19 @@ class FavoriteRestaurantSearchView {
       document.getElementById('restaurant-search-container')
         .dispatchEvent(new Event('restaurant:searched:updated'));
     }
+
+    showFavoriteRestaurants(restaurants = []) {
+        let html;
+        if (restaurants.length) {
+          html = restaurants.reduce((carry, movie) => carry.concat(createMovieItemTemplate(movie)), '');
+        } else {
+          html = '<div class="movie-item__not__found"></div>';
+        }
+    
+        document.getElementById('restaurants').innerHTML = html;
+    
+        document.getElementById('restaurants').dispatchEvent(new Event('restaurants:updated'));
+      }
   }
   
 export default FavoriteRestaurantSearchView;
